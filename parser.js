ea7deee88ff6340f624cd4f6fe599aae31c9340b
Danylo <rdv.romanov@gmail.com>
2018-02-25 11:58:31 +0200

    Added Parser and other stuff

---
+++ parser.js
@@ -0,0 +1,33 @@
+"use strict";
+const http = require('http');
+const htmlparser = require('htmlparser2');
+const hostname = 'www.sinfest.net';
+
+function ParseTitlePage(callback) {
+    const parser = new htmlparser.Parser({
+        onopentag: (name, attr) => {
+            if (name === 'img' && attr.src.indexOf('btphp') !== -1) {
+                console.log(`Parsed url:${attr.src}`);
+                callback(hostname + '/' + attr.src);
+            }
+        }
+    }, {
+        decodeEntities: true
+    });
+    const options = {
+        hostname: 'www.sinfest.net',
+        path: '/index.php',
+        method: 'GET',
+        headers: {
+            'user-agent': 'Mozilla/5.0'
+        }
+    };
+    let data = '';
+    http.get(options, (res) => {
+        res.on('data', (chunk) => parser.write(chunk))
+            .on('end', () => parser.end())
+    }).on('error', (e) => console.error(e));
+
+}
+ParseTitlePage((url) => console.log(url))
+module.exports = ParseTitlePage;
